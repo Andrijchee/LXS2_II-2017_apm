07a09c0391ebb48db4a02003e0463fa0bf3924f5
Andrey Porras M <afpm87@gmail.com>
2017-03-18 16:09:40 -0600

    script parametros

---
+++ src/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+argumentos=3
+
+if [ $# -eq $argumentos ]
+then 
+	echo El numero de parametros fue correcto
+else 
+	echo El numero de parametros fue incorrecto
+	echo se pasaron $# argumentos
+fi
+
+echo fin de script
+exit 0
