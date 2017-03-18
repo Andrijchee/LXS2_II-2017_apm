aec70e1c3e577c91eccdbed48dd8ced38784e6c8
Andrey Porras M <afpm87@gmail.com>
2017-03-18 16:13:12 -0600

    script parametros perro

--- src/script.sh
+++ src/script.sh
@@ -5,6 +5,10 @@ argumentos=3
 if [ $# -eq $argumentos ]
 then 
 	echo El numero de parametros fue correcto
+	if [ $1 = perro ]
+	then 
+		echo EL parametro '$1' fue $1
+	fi
 else 
 	echo El numero de parametros fue incorrecto
 	echo se pasaron $# argumentos
